8cbfd6854c6431506fff10659a61057e44249c20
Daniel Livingston <contact@daniellivingston.net>
2023-05-30 13:37:57 -0700

    Add basic HTML

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Subscribe to our newsletter!</title>
+	<meta
+		name="description"
+		content="Join 60,000 product managers receiving monthly updates on product discovery, measuring success, and much more!"
+	/>
+</svelte:head>
+
+<main>
+	<h1>Stay updated!</h1>
+
+	<p>Join 60,000 product managers receiving monthly updates on:</p>
+
+	<ul>
+		<li>Product discovery and building what matters</li>
+
+		<li>Measuring to ensure updates are a success</li>
+
+		<li>And much more!</li>
+	</ul>
+
+	<form action="">
+		<label for="email">Email address</label>
+		<input type="email" name="email" id="email" placeholder="email@company.com" />
+
+		<button type="submit">Subscribe to monthly newsletter</button>
+	</form>
+</main>
