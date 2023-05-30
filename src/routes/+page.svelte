c21fa293ef7ff6601c81ad7d1541e78719d96c8f
Daniel Livingston <contact@daniellivingston.net>
2023-05-30 13:53:36 -0700

    Basic mobile styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,22 +7,93 @@
 </svelte:head>
 
 <main>
-	<h1>Stay updated!</h1>
+	<img src="/illustration-sign-up-mobile.svg" alt="" />
 
-	<p>Join 60,000 product managers receiving monthly updates on:</p>
+	<div class="content">
+		<h1>Stay updated!</h1>
 
-	<ul>
-		<li>Product discovery and building what matters</li>
+		<p>Join 60,000 product managers receiving monthly updates on:</p>
 
-		<li>Measuring to ensure updates are a success</li>
+		<ul>
+			<li>Product discovery and building what matters</li>
 
-		<li>And much more!</li>
-	</ul>
+			<li>Measuring to ensure updates are a success</li>
 
-	<form action="">
-		<label for="email">Email address</label>
-		<input type="email" name="email" id="email" placeholder="email@company.com" />
+			<li>And much more!</li>
+		</ul>
 
-		<button type="submit">Subscribe to monthly newsletter</button>
-	</form>
+		<form action="">
+			<label for="email">Email address</label>
+			<input type="email" name="email" id="email" placeholder="email@company.com" />
+
+			<button type="submit">Subscribe to monthly newsletter</button>
+		</form>
+	</div>
 </main>
+
+<style>
+	div.content {
+		padding: 1.5rem;
+	}
+
+	h1 {
+		font-size: 2.5rem;
+		font-weight: 700;
+	}
+
+	p {
+		margin-top: 1.25rem;
+	}
+
+	ul {
+		list-style: none;
+		margin-top: 1.25rem;
+	}
+
+	li {
+		position: relative;
+		padding-left: 2rem;
+	}
+
+	li + li {
+		margin-top: 0.75rem;
+	}
+
+	li::before {
+		content: url('/icon-list.svg');
+		position: absolute;
+		left: 0;
+	}
+
+	form {
+		margin-top: 2rem;
+	}
+
+	label {
+		font-size: 0.75rem;
+		font-weight: 700;
+	}
+
+	input {
+		border: 1px solid var(--grey);
+		border-radius: 0.5rem;
+		color: var(--charcoal);
+		font-size: 1rem;
+		margin-top: 0.5rem;
+		outline: none;
+		padding: 1rem 1.25rem;
+		width: 100%;
+	}
+
+	button {
+		background-color: var(--dark-slate);
+		border: 1px solid var(--dark-slate);
+		border-radius: 0.5rem;
+		color: var(--white);
+		font-size: 1rem;
+		font-weight: bold;
+		margin-top: 1.5rem;
+		padding: 1rem 1.25rem;
+		width: 100%;
+	}
+</style>
